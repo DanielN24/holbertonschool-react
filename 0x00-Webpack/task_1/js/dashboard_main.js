da97e7ef9f442321ca38d5c321e34b15d770eab8
DanielN24 <capdaniel_98@outlook.com>
2022-12-26 21:07:56 -0500

    new files with the answer number 1

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -6,7 +6,7 @@ $('body').append('<button>Click here to get started</button>');
 $('body').append("<p id='count'></p>");
 $('body').append('<p>Copyright - Holberton School</p>');
 
-let clickCount = 1;
+let clickCount = 0;
 
 function updateCounter(){
     clickCount += 1;
